8c7e99372057cc9daaaecabd63a7c48d6f33a6dc
Ngo Van Tai <internit00@okxe.vn>
2020-12-29 01:45:15 +0700

    update database.js

--- utils/database.js
+++ utils/database.js
@@ -1,24 +1,22 @@
 const mysql = require('mysql')
+var pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'elearning',
+  connectionLimit: 50,
+})
 module.exports = {
   load(sql){
     return new Promise(
       function(done, fail){
-        var connection = mysql.createConnection({
-          host: 'localhost',
-          user: 'root',
-          password: 'root',
-          database: 'elearning'
-        })
-        connection.connect();
-        connection.query(sql, function (error, results, fields){
+        pool.query(sql, function (error, results, fields){
           if(error)
             fail(error)
           else{
             done(results)
           }
-        
-          connection.end();
-          })
+        })
       }
     )
   }
