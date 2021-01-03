194c8a49d27248cf41fced40d914aecb03c640ae
hoangvietnguyen99 <hoangvietnguyen99@gmail.com>
2021-01-03 13:58:35 +0700

    Add authentication view

--- app.js
+++ app.js
@@ -1,26 +1,13 @@
-require('dotenv').config()
-
-const createError = require('http-errors');
 const express = require('express');
-// const port = 8000;
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const exphbs  = require('express-handlebars');
 const bodyParser = require('body-parser')
-
-const pageRouter = require('./routes/pages')
-const adminRouter = require('./routes/admin')
 
 const app = express();
 
 // view engine setup
-const hbs = exphbs.create({
-	extname: 'hbs',
-});
-app.set('views', path.join(__dirname, 'views'));
-app.engine("hbs", hbs.engine);
-app.set("view engine", "hbs");
+require('./middlewares/view')(app);
 // end view set up
 
 // parse application/json
@@ -33,31 +20,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //router
-app.use('/', pageRouter);
-app.use('/admin', adminRouter);
+require('./middlewares/route')(app);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-	next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
-	// render the error page
-	res.status(err.status || 500);
-	if (err.status === 403) res.render('403', {
-		layout: false
-	});
-	else if (err.status === 404) res.render('404', {
-		layout: false
-	});
-	else res.render('500', {
-			layout: false
-		});
-});
+require('./middlewares/error')(app);
 
 module.exports = app;
 
