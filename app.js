55cc13d80f8a0a8db2d0e60bf5c21f7515547fc3
Ngo Van Tai <internit00@okxe.vn>
2021-01-05 12:19:15 +0700

    oke

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
+<<<<<<< HEAD
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const bodyParser = require('body-parser')
+=======
+const createError = require('http-errors');
+const express = require('express');
+const port = 8000;
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const exphbs  = require('express-handlebars');
+
+const pageRouter = require('./routes/pages')
+const adminRouter = require('./routes/admin')
+>>>>>>> parent of d616815... add 400 500 page
 
 const app = express();
 
 // view engine setup
+<<<<<<< HEAD
 require('./middlewares/view')(app);
 // end view set up
 
@@ -16,16 +30,46 @@ app.use(bodyParser.json())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+=======
+const hbs = exphbs.create({
+    extname: 'hbs',
+});
+app.set('views', path.join(__dirname, 'views'));
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+>>>>>>> parent of d616815... add 400 500 page
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //router
+<<<<<<< HEAD
 require('./middlewares/route')(app);
 
 require('./middlewares/error')(app);
+=======
+app.use('/', pageRouter);
+app.use('/admin', adminRouter);
 
-module.exports = app;
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+	next(createError(404));
+});
 
-// app.listen(port, () => {
-// 	console.log(`Example app listening at http://localhost:${port}`)
-// })
+// error handler
+app.use(function(err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
+});
+>>>>>>> parent of d616815... add 400 500 page
+
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`)
+})
