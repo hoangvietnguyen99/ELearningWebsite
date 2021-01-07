e1bde96f5b4686d1f67d5c4412b617e2fbe0ed4f
hoangvietnguyen99 <hoangvietnguyen99@gmail.com>
2021-01-08 00:53:31 +0700

    add category field

---
+++ models/field_course.model.js
@@ -0,0 +1,18 @@
+const database = require('../utils/database');
+
+const TBL_FIELD_COURSE = 'field_course';
+
+module.exports = {
+	async addOne(fieldId, courseId, connection) {
+		const result = await database.add({fieldid: fieldId, courseid: courseId}, TBL_FIELD_COURSE, connection);
+		return result.affectedRows;
+	},
+	async removeOne(fieldId, courseId, connection) {
+		const result = await database.delete([{fieldid: fieldId}, {courseid: courseId}], TBL_FIELD_COURSE, connection);
+		return result.affectedRows;
+	},
+	async getListCourseIdsByFieldId(fieldId, connection) {
+		const query = `SELECT courseid FROM ${TBL_FIELD_COURSE} WHERE fieldid = ${fieldId}`;
+		return await database.query(query, connection);
+	}
+}
