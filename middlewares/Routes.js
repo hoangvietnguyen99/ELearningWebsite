b16a20587098e9e6fef49099e14cd85d8e18b779
Ngo Van Tai <internit00@okxe.vn>
2021-01-08 09:53:11 +0700

    update

--- middlewares/Routes.js
+++ middlewares/Routes.js
@@ -1,37 +1,37 @@
 const auth = require('./Authentication');
-const {Auth} = require("./Validator");
+const { Auth } = require("./Validator");
 
-module.exports = function (app) {
-  // app.get('/', function (req, res) {
-  //   // res.send('Hello World!');
-  //   // console.log(res.locals.lcCategories);
-  //   res.render('home');
-  // });
+module.exports = function(app) {
+    // app.get('/', function (req, res) {
+    //   // res.send('Hello World!');
+    //   // console.log(res.locals.lcCategories);
+    //   res.render('home');
+    // });
 
-  // app.get('/about', function (req, res) {
-  //   throw new Error('ABOUT BROKEN');
-  //   res.render('about');
-  // });
+    // app.get('/about', function (req, res) {
+    //   throw new Error('ABOUT BROKEN');
+    //   res.render('about');
+    // });
 
-  // app.get('/bs4', function (req, res) {
-  //   // res.sendFile(`${__dirname}/bs4.html`);
+    // app.get('/bs4', function (req, res) {
+    //   // res.sendFile(`${__dirname}/bs4.html`);
 
-  //   const show = +req.query.show || 0;
-  //   const visible = show !== 0;
+    //   const show = +req.query.show || 0;
+    //   const visible = show !== 0;
 
-  //   res.render('bs4', {
-  //     layout: false,
-  //     data: { visible: visible }
-  //   });
-  // });
+    //   res.render('bs4', {
+    //     layout: false,
+    //     data: { visible: visible }
+    //   });
+    // });
 
-  // app.use('/admin/categories', require('../routes/category.route'));
-  // app.use('/admin/products', require('../routes/product.route'));
+    app.use('/admin', require('../routes/admin'));
+    // app.use('/admin/products', require('../routes/product.route'));
 
-  app.use('/', require('../routes/pages'));
-  app.use('/auth', require('../routes/Authentication'));
-  // app.use('/products', require('../routes/front/product.route'));
-  // app.use('/cart', auth, require('../routes/front/cart.route'));
+    app.use('/', require('../routes/pages'));
+    app.use('/auth', require('../routes/Authentication'));
+    // app.use('/products', require('../routes/front/product.route'));
+    // app.use('/cart', auth, require('../routes/front/cart.route'));
 
-  // app.use('/demo', require('../routes/demo.route'));
+    // app.use('/demo', require('../routes/demo.route'));
 }
