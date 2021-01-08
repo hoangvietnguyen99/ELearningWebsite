7d72da74b1a6527094a2ee91a59e9575f4416803
nntrung <nntrung0403@gmail.com>
2021-01-08 11:40:24 +0700

    upload image

--- controllers/client/editcourses.controller.js
+++ controllers/client/editcourses.controller.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const coursesModel = require('../../models/course.model');
+const multer = require('multer');
 
 exports.getCourses  = async function(req,res,next ){ 
     const user = req.session.authUser;
@@ -10,4 +11,24 @@ exports.getCourses  = async function(req,res,next ){
         courses : courses,
         empty: courses.length == 0
     });
+};
+
+exports.addImage =  function(req,res,next ){ 
+    const storage = multer.diskStorage({
+		destination: function (req, file, cb) {
+		  cb(null, './public/assets/client/images/courses')
+		},
+		filename: function (req, file, cb) {
+		  cb(null, req.params.id + `.png`)
+		}
+	  });
+	  const upload = multer({ storage });
+	  // upload.single('fuMain')(req, res, function (err) {
+	  upload.array('fuMain',1)(req, res, function (err) {
+		if (err) {
+			console.log('Error load');
+		} else {
+			res.render('clients/editor', { layout: 'layoutclient.hbs' });
+		}
+	  });
 };
