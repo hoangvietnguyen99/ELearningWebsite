a039bbf277a85ddcd9ba9e7f01f25ca67d74dee5
nntrung <nntrung0403@gmail.com>
2021-01-12 13:23:54 +0700

    update profile

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -2,9 +2,10 @@ const userModel = require('../../models/user.model');
 
 exports.getUserByID = async function(req,res){
     const user = await userModel.getById(req.params.id);
+    console.log(user.role);
     res.render('clients/profile',{
         layout: 'layoutclient.hbs',
         user: user,
-        isTeacher: user.role == "Teacher"
+        isTeacher: user.role === "TEACHER"
     })
 }
