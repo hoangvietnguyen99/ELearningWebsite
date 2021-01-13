5a80d07dde3793679df3500e6808e860253b4cec
Ngo Van Tai <internit00@okxe.vn>
2021-01-13 16:53:12 +0700

    full-text search

--- controllers/client/course.controller.js
+++ controllers/client/course.controller.js
@@ -8,104 +8,106 @@ const reviewModel = require('../../models/review.model');
 const fieldModel = require('../../models/field.model');
 
 module.exports = {
-	async getAllAvailable(req, res) {
-		let pageIndex = req.query.pageIndex || 1;
-		const field = req.query.fieldid ? await fieldModel.getById(req.query.fieldid) : null;
-		const userId = req.session.authUser ? req.session.authUser.id : null;
-		if (pageIndex == 0) pageIndex = 1;
-		const pageSize = req.query.pageSize || 12;
-		let [count, courses, userCourseIds, uploadIds] = await Promise.all([
-			field ? 0 : courseModel.getCountAvailable(),
-			field ? courseModel.getAvailableCoursesByIds(await field_courseModel.getListCourseIdsByFieldId(field.id)) : courseModel.getAllAvailable(null, pageIndex, pageSize),
-			userId ? user_courseModel.getCourseIdsByUserId(userId) : [],
-			courseModel.getCourseIdsByAuthorId(userId)
-		]);
-		if (field) count = courses.length;
-		const totalPages = Math.ceil(count / pageSize);
-		res.render('clients/courses', {
-			layout: 'layoutclient',
-			data: {
-				courses: field ? courses.slice(pageSize*(pageIndex - 1),pageSize*pageIndex) : courses,
-				title: field ? field.name : null,
-				fieldId: field ? field.id : null,
-				count,
-				pageIndex,
-				pageSize,
-				totalPages,
-				userCourseIds,
-				uploadIds
-			}
-		});
-	},
+    async getAllAvailable(req, res) {
+        let keyword = req.query.keyword || null
+        let pageIndex = req.query.pageIndex || 1;
+        const field = req.query.fieldid ? await fieldModel.getById(req.query.fieldid) : null;
+        const userId = req.session.authUser ? req.session.authUser.id : null;
+        if (pageIndex == 0) pageIndex = 1;
+        const pageSize = req.query.pageSize || 12;
+        let [count, courses, userCourseIds, uploadIds] = await Promise.all([
+            field ? 0 : courseModel.getCountAvailable(),
+            field ? courseModel.getAvailableCoursesByIds(await field_courseModel.getListCourseIdsByFieldId(field.id)) : courseModel.getAllAvailable(null, pageIndex, pageSize, keyword),
+            userId ? user_courseModel.getCourseIdsByUserId(userId) : [],
+            courseModel.getCourseIdsByAuthorId(userId)
+        ]);
+        if (field) count = courses.length;
+        const totalPages = Math.ceil(count / pageSize);
+        res.render('clients/courses', {
+            layout: 'layoutclient',
+            data: {
+                courses: field ? courses.slice(pageSize * (pageIndex - 1), pageSize * pageIndex) : courses,
+                title: field ? field.name : null,
+                fieldId: field ? field.id : null,
+                count,
+                pageIndex,
+                pageSize,
+                totalPages,
+                userCourseIds,
+                uploadIds
+            }
+        });
+    },
 
-	async addReview(req, res) {
-		const connection = await database.getConnection();
-		connection.beginTransaction(async err => {
-			if (err) throw err;
-			try {
-				const thisCourse = courseModel.getById(req.params.courseid, connection);
-				if (!thisCourse) throw `Course not found: ${req.body.courseid}`;
-				if (!await userModel.hasCourseIdCheck(req.session.authUser.id, thisCourse.id, connection)) throw `User has not buy the course`;
-				if (await reviewModel.hasRatedCheck(req.session.authUser.id, thisCourse.id, connection)) throw `User has reviewd, only one is allowed per user`;
-				const review = {
-					userid: req.session.authUser.id,
-					courseid: req.params.courseid,
-					createddat: new Date(),
-					point: parseFloat(req.body.point) || 5,
-					comment: req.body.comment
-				}
-				const result = await reviewModel.addOne(review, connection);
-				if (result) {
-					const reviews = await reviewModel.getAllByCourseId(thisCourse.id, connection);
-					const totalRating = reviews.reduce((a, b) => a + b.point, 0);
-					const rating = totalRating / reviews.length;
-					thisCourse.commentscount = reviews.length;
-					thisCourse.rating = rating;
-					const result = await courseModel.update(thisCourse, connection);
-					if (result) {
-						connection.commit(commitError => {
-							connection.release();
-							if (commitError) throw commitError;
-							res.redirect(req.headers.referer || '/');
-						});
-					} else throw 'Can not save course';
-				} else throw "Can not create review";
-			} catch (err) {
-				console.log(err);
-				connection.rollback(rollbackError => {
-					connection.release();
-					if (rollBackError) throw rollbackError;
-					res.redirect(req.headers.referer || '/');
-				});
-			}
-		});
-	},
+    async addReview(req, res) {
+        const connection = await database.getConnection();
+        connection.beginTransaction(async err => {
+            if (err) throw err;
+            try {
+                const thisCourse = courseModel.getById(req.params.courseid, connection);
+                if (!thisCourse) throw `Course not found: ${req.body.courseid}`;
+                if (!await userModel.hasCourseIdCheck(req.session.authUser.id, thisCourse.id, connection)) throw `User has not buy the course`;
+                if (await reviewModel.hasRatedCheck(req.session.authUser.id, thisCourse.id, connection)) throw `User has reviewd, only one is allowed per user`;
+                const review = {
+                    userid: req.session.authUser.id,
+                    courseid: req.params.courseid,
+                    createddat: new Date(),
+                    point: parseFloat(req.body.point) || 5,
+                    comment: req.body.comment
+                }
+                const result = await reviewModel.addOne(review, connection);
+                if (result) {
+                    const reviews = await reviewModel.getAllByCourseId(thisCourse.id, connection);
+                    const totalRating = reviews.reduce((a, b) => a + b.point, 0);
+                    const rating = totalRating / reviews.length;
+                    thisCourse.commentscount = reviews.length;
+                    thisCourse.rating = rating;
+                    const result = await courseModel.update(thisCourse, connection);
+                    if (result) {
+                        connection.commit(commitError => {
+                            connection.release();
+                            if (commitError) throw commitError;
+                            res.redirect(req.headers.referer || '/');
+                        });
+                    } else throw 'Can not save course';
+                } else throw "Can not create review";
+            } catch (err) {
+                console.log(err);
+                connection.rollback(rollbackError => {
+                    connection.release();
+                    if (rollBackError) throw rollbackError;
+                    res.redirect(req.headers.referer || '/');
+                });
+            }
+        });
+    },
 
-	async getCourse(req, res){
-		const thisCourse = await courseModel.getById(req.params.id);
-		thisCourse.viewscount++;
-		await courseModel.update(thisCourse);
-		let userCourseIds = [];
-		if (req.session.authUser) {
-			userCourseIds = await user_courseModel.getCourseIdsByUserId(req.session.authUser.id);
-		}
-		const author = await userModel.getById(thisCourse.author);
-		const lessons = await lessonModel.getAllByCourseId(thisCourse.id);
-		const reviews = await reviewModel.getAllByCourseId(thisCourse.id);
-		let isInCart = false;
-		const found = req.session.authUser ? res.locals.cart.courses.find(course => course.id === thisCourse.id) : null;
-		if (found) isInCart = true;
-		res.render('clients/course', {
-			layout: 'layoutclient.hbs',
-			data: {
-				hasThisCourse: userCourseIds.includes(thisCourse.id),
-				isInCart,
-				thisCourse,
-				author,
-				isAuthor: req.session.authUser ? req.session.authUser.id === author.id : false,
-				lessons,
-				reviews
-			}
-		});
-	}
+    async getCourse(req, res) {
+        const thisCourse = await courseModel.getById(req.params.id);
+        thisCourse.viewscount++;
+        await courseModel.update(thisCourse);
+        let userCourseIds = [];
+        if (req.session.authUser) {
+            userCourseIds = await user_courseModel.getCourseIdsByUserId(req.session.authUser.id);
+        }
+        const author = await userModel.getById(thisCourse.author);
+        const lessons = await lessonModel.getAllByCourseId(thisCourse.id);
+        const reviews = await reviewModel.getAllByCourseId(thisCourse.id);
+        let isInCart = false;
+        const found = req.session.authUser ? res.locals.cart.courses.find(course => course.id === thisCourse.id) : null;
+        if (found) isInCart = true;
+        res.render('clients/course', {
+            layout: 'layoutclient.hbs',
+            data: {
+                hasThisCourse: userCourseIds.includes(thisCourse.id),
+                isInCart,
+                thisCourse,
+                author,
+                isAuthor: req.session.authUser ? req.session.authUser.id === author.id : false,
+                lessons,
+                reviews
+            }
+        });
+    },
+
 }
--- routes/courses.js
+++ routes/courses.js
@@ -1,26 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
-const {isAuth} = require('../middlewares/Authentication');
+const { isAuth } = require('../middlewares/Authentication');
 const courseController = require('../controllers/client/course.controller');
 const lessonController = require('../controllers/client/lesson.controller');
 
 router.route('/')
-	.get(courseController.getAllAvailable);
+    .get(courseController.getAllAvailable);
 
 router.route('/:id')
-	.get(courseController.getCourse)
+    .get(courseController.getCourse)
 
 router.route('/:courseid/reviews')
-	.post(isAuth, courseController.addReview);
+    .post(isAuth, courseController.addReview);
 
 router.route('/:id/lessons')
-	.post(lessonController.addLesson)
+    .post(lessonController.addLesson)
 
 
 router.route('/:id/lessons/:lid')
-	.delete(lessonController.deleteLesson)
-	.put(lessonController.editLesson)
-	.post(lessonController.addVideo)
+    .delete(lessonController.deleteLesson)
+    .put(lessonController.editLesson)
+    .post(lessonController.addVideo)
 
 module.exports = router;
